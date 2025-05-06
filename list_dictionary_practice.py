11f9415612eefa6dfe085e16d758bda3e33afb6e
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-05-06 18:58:54 +0500

    Add files via upload

---
+++ list_dictionary_practice.py
@@ -0,0 +1,13 @@
+#  Part1 Lists
+fruits = ["apple","orange","grapes"]
+print("fruits list",fruits)
+print("fruit name",fruits[1])
+fruits.append("pineapple")
+print("updated fruits list",fruits)
+
+# Part2 Dictionary
+student ={"name":"Fiza Arzoo Khalid","age": 26, "education":"Bachelor's"}
+print("Student Info:",student)
+print("Name:", student["name"]) 
+student["university"]="Punjab university Lahore"
+print("updated Student Info:",student)
